70be3bc15722053099de63a604ec8e78da2ad5c7
John Owen <john.owen@dabble.com.au>
2024-08-09 23:24:28 +1000

    changes

--- terrain_3d/terrain_3d.gd
+++ terrain_3d/terrain_3d.gd
@@ -49,80 +49,75 @@ var _image_width: int:
 		return mesh_subdivisions + 2
 
 
-var _initial_height_map_image: Image
-var _height_map_image: Image
-var _height_map_texture: ImageTexture
-var _collision_shape: CollisionShape3D
+var _initial_height_image: Image
+var _height_image: Image
+var _height_texture: ImageTexture
+var _height_map_shape: HeightMapShape3D
+
+
+@onready var _mesh: MeshInstance3D = $MeshInstance3D
+@onready var _plane_mesh: PlaneMesh = _mesh.mesh
+@onready var _collision_shape: CollisionShape3D = $CollisionShape3D
 
 
 func _ready() -> void:
 	_regen()
 
 
+func _notification(what: int) -> void:
+	match what:
+		# We do this so we don't clog git with unecessary data
+		NOTIFICATION_EDITOR_PRE_SAVE:
+			_MATERIAL.set_shader_parameter("height_texture", null)
+			_collision_shape.shape = null
+		NOTIFICATION_EDITOR_POST_SAVE:
+			_MATERIAL.set_shader_parameter("height_texture", _height_texture)
+			_collision_shape.shape = _height_map_shape
+
+
 func _regen() -> void:
-	for child in get_children():
-		remove_child(child)
-		child.queue_free()
-	var plane := PlaneMesh.new()
-	plane.size = Vector2.ONE
-	plane.subdivide_width = mesh_subdivisions
-	plane.subdivide_depth = mesh_subdivisions
-	var mesh := MeshInstance3D.new()
-	mesh.scale = Vector3(length, 1.0, length)
-	# Prevent mesh disappearing when player is deep underground
-	mesh.extra_cull_margin = 16384.0
-	mesh.mesh = plane
-	# Duplicate material so we don't end up persisting the height texture in git
-	var material: ShaderMaterial = _MATERIAL.duplicate()
-	mesh.material_override = material
-	add_child(mesh)
-	material.set_shader_parameter("height_scale", max_height)
-	_height_map_image = _HEIGHT_MAP.get_image()
-	if _height_map_image == null:
+	_mesh.scale = Vector3(length, 1.0, length)
+	_plane_mesh.subdivide_width = mesh_subdivisions
+	_plane_mesh.subdivide_depth = mesh_subdivisions
+	_height_image = _HEIGHT_MAP.get_image()
+	if _height_image == null:
 		await _HEIGHT_MAP.changed
-		_height_map_image = _HEIGHT_MAP.get_image()
+		_height_image = _HEIGHT_MAP.get_image()
 	# .get_image() should return a copy of the data according to the docs but it
 	# seems to return a reference so we use use get_region to actually copy
-	_height_map_image = _height_map_image.get_region(
-		_height_map_image.get_used_rect()
+	_height_image = _height_image.get_region(_height_image.get_used_rect())
+	_height_image.resize(
+		_image_width, _image_width, Image.Interpolation.INTERPOLATE_CUBIC
 	)
-	_height_map_image.resize(
-		_image_width,
-		_image_width,
-		Image.Interpolation.INTERPOLATE_CUBIC
-	)
-	_height_map_image.convert(Image.FORMAT_RF)
+	_height_image.convert(Image.FORMAT_RF)
 	for x in _image_width:
 		for y in _image_width:
-			var r_old := _height_map_image.get_pixel(x, y).r
+			var r_old := _height_image.get_pixel(x, y).r
 			var height := remap(
 				r_old, 0.0, 1.0, min_initial_height, max_initial_height
 			)
 			var r_new := height / max_height
-			_height_map_image.set_pixel(x, y, Color(r_new, 0.0, 0.0, 1.0))
-	_collision_shape = CollisionShape3D.new()
+			_height_image.set_pixel(x, y, Color(r_new, 0.0, 0.0, 1.0))
 	_collision_shape.scale = length / float(mesh_subdivisions + 1) * Vector3.ONE
-	var map_data := _height_map_image.get_data().to_float32_array()
+	var map_data := _height_image.get_data().to_float32_array()
 	for i in map_data.size():
 		map_data[i] *= max_height / _collision_shape.scale.x
-	var height_map_shape := HeightMapShape3D.new()
-	height_map_shape.map_width = _image_width
-	height_map_shape.map_depth = _image_width
-	height_map_shape.map_data = map_data
-	_collision_shape.shape = height_map_shape
-	add_child(_collision_shape)
-	_height_map_texture = ImageTexture.create_from_image(_height_map_image)
-	material.set_shader_parameter(
-		"texture_height", _height_map_texture
-	)
-	_initial_height_map_image = _height_map_image.get_region(
-		_height_map_image.get_used_rect()
+	_height_map_shape = HeightMapShape3D.new()
+	_height_map_shape.map_width = _image_width
+	_height_map_shape.map_depth = _image_width
+	_height_map_shape.map_data = map_data
+	_collision_shape.shape = _height_map_shape
+	_height_texture = ImageTexture.create_from_image(_height_image)
+	_MATERIAL.set_shader_parameter("height_scale", max_height)
+	_MATERIAL.set_shader_parameter("texture_height", _height_texture)
+	_initial_height_image = _height_image.get_region(
+		_height_image.get_used_rect()
 	)
 
 
 func get_height_at_position(point: Vector3) -> float:
 	var point_image := _world_space_to_image_space(point)
-	var r := _height_map_image.get_pixelv(point_image.floor()).r
+	var r := _height_image.get_pixelv(point_image.floor()).r
 	return r * max_height
 
 
@@ -143,19 +138,18 @@ func dig(point: Vector3, radius: float, dig_depth: float) -> void:
 			var smoothed_dig_depth := dig_depth * smoothstep(
 				0.0, 1.0, 1.0 - float(point_dist) / float(radius_image)
 			)
-			var old_r := _height_map_image.get_pixel(x, y).r
-			var initial_r := _initial_height_map_image.get_pixel(x, y).r
+			var old_r := _height_image.get_pixel(x, y).r
+			var initial_r := _initial_height_image.get_pixel(x, y).r
 			var old_height := old_r * max_height
 			var initial_height := initial_r * max_height
 			var new_height := maxf(
-				old_height - smoothed_dig_depth,
-				initial_height - max_dig_depth
+				old_height - smoothed_dig_depth, initial_height - max_dig_depth
 			)
 			var new_r := new_height / max_height
-			_height_map_image.set_pixel(x, y, Color(new_r, 0.0, 0.0, 1.0))
+			_height_image.set_pixel(x, y, Color(new_r, 0.0, 0.0, 1.0))
 			var data_index := (_image_width) * y + x
 			map_data[data_index] = new_height / _collision_shape.scale.x
-	_height_map_texture.update(_height_map_image)
+	_height_texture.update(_height_image)
 	height_map_shape.map_data = map_data
 
 
